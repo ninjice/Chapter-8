a0b50d318e3235d9603201e3ce71931d4f858f81
Ninjice <31352846+ninjice@users.noreply.github.com>
2018-04-02 16:30:00 -0700

    4:29 PM

--- Lab2/__SHELL2.java
+++
@@ -1,8 +0,0 @@
-package Lab2;
-public class __SHELL2 extends bluej.runtime.Shell {
-public static void run() throws Throwable {
-
-java.lang.String[] __bluej_param0 = { };
-Lab2.IntegerListSTest.main(__bluej_param0);
-
-}}
